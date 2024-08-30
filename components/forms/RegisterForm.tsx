1c31d3a7492cf9ffa473b65e8ee58fcfd6116bc3
cesar eduardo <ceed1000@gmail.com>
2024-08-30 15:50:09 -0500

    hora con 26 minutos ahi un error con los estilos

---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useState } from "react";
+import { Form } from "@/components/ui/form";
+import CustomFormField from "../CustomFormField";
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
+import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
+
+export enum FormFieldType {
+  INPUT = "input",
+  TEXTAREA = "textarea",
+  PHONE_INPUT = "phoneInput",
+  CHECKBOX = "checkbox",
+  DATE_PICKER = "datePicker",
+  SELECT = "select",
+  SKELETON = "skeleton",
+}
+
+const RegisterForm = ({ user }: { user: User }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit({
+    name,
+    email,
+    phone,
+  }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true);
+
+    console.log("onSubmit function called"); // Prueba de que se ejecuta onSubmit
+
+    try {
+      const userData = { name, email, phone };
+      console.log("Creating user with data:", userData); // Datos del usuario
+
+      const user = await createUser(userData);
+
+      if (user) {
+        console.log("User created:", user); // Verifica si se crea el usuario
+        router.push(`/patients/${user.$id}/register`);
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+
+    setIsLoading(false);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit((data) => {
+          console.log("Form submitted with data:", data); // Prueba de que el form está validado y se llama a handleSubmit
+          onSubmit(data);
+        })}
+        className="space-y-6 flex-1"
+      >
+        <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Schedule your first appointment.</p>
+        </section>
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="name"
+          label="full name"
+          placeholder="John doe"
+          iconSrc="/assets/icons/user.svg"
+          iconAlt="user"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.INPUT}
+          control={form.control}
+          name="email"
+          label="Email"
+          placeholder="cesar@ejemplo.com"
+          iconSrc="/assets/icons/email.svg"
+          iconAlt="email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="(555) 123-4567"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+      </form>
+    </Form>
+  );
+};
+
+export default RegisterForm;
