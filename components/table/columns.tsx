79e5d691c9be77c1cfb9d022461d37eed0311349
WilsonCollantesAponte <wilson.aponte000@gmail.com>
2024-09-28 23:54:33 -0500

    4 horas con 17 minutos

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -2,23 +2,21 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreHorizontal } from "lucide-react";
-
+import Image from "next/image";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
+import StatusBadge from "../StatusBadge";
+import { formatDateTime } from "@/lib/utils";
+import { Doctors } from "@/constants";
+import AppointmentModal from "../AppointmentModal";
 export type Payment = {
   id: string;
   amount: number;
   status: "pending" | "processing" | "success" | "failed";
   email: string;
+  patient: string;
 };
 
 export const columns: ColumnDef<Payment>[] = [
@@ -27,51 +25,75 @@ export const columns: ColumnDef<Payment>[] = [
     cell: ({ row }) => <p className="text-14-medium">{row.index + 1}</p>,
   },
   {
+    accessorKey: "patient",
+    header: "Patient",
+    cell: ({ row }) => (
+      <p className="text-14-medium">{row.original.paciente.name}</p>
+    ),
+  },
+
+  {
     accessorKey: "status",
     header: "Status",
+    cell: ({ row }) => (
+      <div className="min-w-[115px]">
+        <StatusBadge status={row.original.status} />
+      </div>
+    ),
   },
-  {
-    accessorKey: "email",
-    header: "Email",
-  },
-  {
-    accessorKey: "amount",
-    header: () => <div className="text-right">Amount</div>,
-    cell: ({ row }) => {
-      const amount = parseFloat(row.getValue("amount"));
-      const formatted = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(amount);
 
-      return <div className="text-right font-medium">{formatted}</div>;
-    },
-  },
   {
-    id: "actions",
+    accessorKey: "schedule",
+    header: "Appointment",
+    cell: ({ row }) => (
+      <p className="text-14-regular min-w-[100px]">
+        {formatDateTime(row.original.schedule).dateTime}
+      </p>
+    ),
+  },
+
+  {
+    accessorKey: "primaryPhysician",
+    header: () => "Doctor",
     cell: ({ row }) => {
-      const payment = row.original;
+      const doctor = Doctors.find(
+        (doc) => doc.name === row.original.primaryPhysician
+      );
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex items-center gap-3">
+          <Image
+            src={doctor?.image}
+            alt={doctor.name}
+            width={100}
+            height={100}
+            className="size-8"
+          />
+          <p className="whitespace-nowrap">Dr. {doctor?.name}</p>
+        </div>
+      );
+    },
+  },
+
+  {
+    id: "actions",
+    header: () => <div className="pl-4">Actions</div>,
+    cell: ({ row: { original: data } }) => {
+      return (
+        <div className="flex gap-1">
+          <AppointmentModal
+            type="schedule"
+            patientId={data.paciente.$id}
+            userId={data.userId}
+            appointmentId={data}
+          />
+          <AppointmentModal
+            type="cancel"
+            patientId={data.paciente.$id}
+            userId={data.userId}
+            appointmentId={data}
+          />
+        </div>
       );
     },
   },
